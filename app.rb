e148bd7e657e17b79c910edae0d23bf9b1a6ad82
TenleyM <tenley.malmquist@gmail.com>
2020-03-16 02:01:03 +0000

    fixed bug

--- app.rb
+++ app.rb
@@ -38,8 +38,8 @@ get "/ski_areas/:id" do
     @average_variety = reviews_table.where(ski_area_id: @ski_area[:id]).avg(:variety_rating)
     @users_table = users_table
     @location = @ski_area[:zipcode]
-    #@geocoder_results = Geocoder.search(@location)
-    #@lat_long = @geocoder_results.first.coordinates
+    @geocoder_results = Geocoder.search(@location)
+    @lat_long = @geocoder_results.first.coordinates
     view "mountain"
 end
 
